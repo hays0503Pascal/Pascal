784dc323d42558e96558bbb15ad9801534249b26
hays0503Pascal <hays0503Pascal@gmail.com>
2016-10-23 17:36:54 +0600

    Update file.pas

--- file.pas
+++ file.pas
@@ -1,1 +1,16 @@
-This pascal file
+program task1;
+var  SomeText:string;NumWord,LengthText,iterator:integer;
+begin
+
+SomeText:='Some Text.';
+LengthText:=Length(SomeText);
+for iterator:=1 to LengthText do
+    begin
+        case SomeText[iterator] of
+        ' ': Inc(NumWord);
+        '.': Inc(NumWord);     
+        end;
+    end;
+writeln('Amount word ', NumWord);
+end.
+
